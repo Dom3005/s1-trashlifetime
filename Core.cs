06e2a080629fb2a0a0e3b17b4a2589181ec206e3
Dom3005 <dominik.goertz.05@gmail.com>
2026-01-05 16:20:18 +0100

    added option to not delete naturally spawned trash

--- Core.cs
+++ Core.cs
@@ -3,21 +3,26 @@ using Il2CppScheduleOne.Trash;
 using MelonLoader;
 using UnityEngine;
 using System.Collections;
+using Il2CppScheduleOne.GameTime;
 
-[assembly: MelonInfo(typeof(S1_TrashLifetime.Core), "TrashLifetime", "1.2.0", "Dom3005", null)]
+[assembly: MelonInfo(typeof(S1_TrashLifetime.Core), "TrashLifetime", "1.3.0", "Dom3005", null)]
 [assembly: MelonGame("TVGS", "Schedule I")]
 
 namespace S1_TrashLifetime
 {
     public class Core : MelonMod
     {
+        public static Core Instance;
+
         public static MelonPreferences_Category menuConfig;
         public static MelonPreferences_Entry<int> timerEntry;
         public static MelonPreferences_Entry<bool> onlyDeleteTrashbagsEntry;
         public static MelonPreferences_Entry<bool> dontDeleteTrashbagsEntry;
+        public static MelonPreferences_Entry<bool> dontDeleteStreettrashEntry;
 
         public override void OnInitializeMelon()
         {
+            Instance = this;
             LoggerInstance.Msg("Initialized!");
             InitializeConfig();
         }
@@ -27,7 +32,8 @@ namespace S1_TrashLifetime
             menuConfig = MelonPreferences.CreateCategory("TrashLifetime", "Trash Lifetime");
             timerEntry = menuConfig.CreateEntry("TrashLifetime", 60, display_name:"Trash Lifetime", description: "Trash Lifetime (seconds), default is 60");
             onlyDeleteTrashbagsEntry = menuConfig.CreateEntry("OnlyDeleteTrashbags", false, display_name: "Only Delete Trashbags", description: "Only affect trash bags (Makes this mod redundant if combined with DontDeleteTrashpacks option). Default: False");
-            dontDeleteTrashbagsEntry = menuConfig.CreateEntry("DontDeleteTrashbags", false, display_name:"Dont delete Trashbags", description: "Do not affect trash bags (Makes this mod redundant if combined with OnlyDeleteTrashpacks option). Default: False");
+            dontDeleteTrashbagsEntry = menuConfig.CreateEntry("DontDeleteTrashbags", false, display_name:"Dont Delete Trashbags", description: "Do not affect trash bags (Makes this mod redundant if combined with OnlyDeleteTrashpacks option). Default: False");
+            dontDeleteStreettrashEntry = menuConfig.CreateEntry("DontDeleteStreettrash", true, display_name:"Dont Delete Streettrash", description: "Do not delete the trash on the streets (it only respawns when sleeping). Default: True");
             menuConfig.SaveToFile();
         }
     }
@@ -38,9 +44,14 @@ namespace S1_TrashLifetime
         private static void Postfix(TrashItem __instance)
         {
             bool isTrashbag = __instance.GetScriptClassName() == "TrashBag";
-            MelonLogger.Msg($"TrashItem detected: {__instance.name}, Trashbag?{isTrashbag}");
             if (Core.onlyDeleteTrashbagsEntry.Value && !isTrashbag) return;
             if (Core.dontDeleteTrashbagsEntry.Value && isTrashbag) return;
+
+            if(TimeManager.Instance != null && Core.dontDeleteStreettrashEntry.Value)
+            {
+                if (Math.Abs(TimeManager.Instance.CurrentTime - TimeManager.SelectedWakeTime) <= 5)
+                    return;
+            }
 
             MelonCoroutines.Start(DeleteTimer(__instance));
         }
